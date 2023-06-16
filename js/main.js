bb163804bb0e7ef8d64a4b030a0da70caf94794c
MericK <morhenac@gmail.com>
2023-06-16 13:09:02 +0300

    new update

--- js/main.js
+++ js/main.js
@@ -2,15 +2,14 @@ import { productsFunc } from "./products-cart.js";
 import header from "./header.js";
 import { wishlistFunc } from "./wishlist.js";
 import search from "./search.js";
-window.onbeforeunload = () => {
+window.onbeforeunload =  () => {
   const getData = (async () => {
     const response = await fetch("/js/data.json");
     const data = await response.json();
-    localStorage.setItem("products", JSON.stringify(data));
+   data ? localStorage.setItem("products", JSON.stringify(data)) :[]
   })();
-};
 
+}
 
-productsFunc();
-wishlistFunc();
-
+wishlistFunc()  
+productsFunc()
