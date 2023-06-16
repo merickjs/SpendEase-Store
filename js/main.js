2c239fc7be61547905553702accaa20a3b3e319f
MericK <morhenac@gmail.com>
2023-06-16 14:01:24 +0300

    tired

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 import { productsFunc } from "./products-cart.js";
 import header from "./header.js";
 import search from "./search.js";
-document.addEventListener("DOMContentLoaded" ,() => {
 
-window.onbeforeunload =  () => {
-  const getData = (async () => {
-    const response = await fetch("/js/data.json");
-    const data = await response.json();
-   data ? localStorage.setItem("products", JSON.stringify(data)) :[]
-  })();
+window.onbeforeunload = () =>{
 
+  const getData =  () => {
+  fetch('/js/data.json')
+  .then(response => response.json())
+  .then(data => localStorage.setItem("products",JSON.stringify(data))
+  )
+  .catch(error => console.error(error));
 }
-
-  productsFunc()
-})
+getData()
+}
+productsFunc()
